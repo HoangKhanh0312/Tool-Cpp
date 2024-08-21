72bf69080db6043a7d94f8a0bca2c4967d400f8d
HoangKhanh0312 <hoangphulamkhanh@gmail.com>
2024-08-21 16:19:52 +0700

    Add files via upload

--- Hoa/Hoa.cpp
+++ Hoa/Hoa.cpp
@@ -1,116 +1,114 @@
 #include <iostream>
+#include <cmath>
 
+void usr();
 void symbol()
 {
-    std::cout<<"<==========>\n";
+    std::cout<<"<=========>\n";
 }
-void symres()
-{
-    std::cout<<"<----------->\n";
-}
-class mol
+class mol 
 {
     public:
-    double smol(double sct)
+    int choose;
+    double show;
+    void usr1()
     {
-    return sct/(6022*std::pow(10,23));
-    }
-    double tinhdotan(double mn,double mct)
-    {
-        return (mct/mn)*100;
-    }
-    double ndpt(double mct,double mdd)
-    {
-        return (mct/mdd)*100;
-    }
-    double ndmol(double mct,double vdd)
-    {
-        return mct/vdd;
+        std::cout<<"1.Tinh khoi luong mol(mol)\n"
+                 <<"2.Tinh so mol\n"
+                 <<"3.Tinh khoi luong mol(g)\n"
+                 <<"4.Tinh the tich mol cua chat khi\n"
+                 <<"5.Tinh so mol dua tren the tich cua chat khi\n"
+                 <<"0.Quay lai\n"
+                 <<"=>";
+        std::cin>>choose;
+        symbol();
+        switch(choose)
+        {
+            case 0:
+            usr();
+            break;
+
+            case 1:
+            double n,m;
+            std::cout<<"Nhap so mol(mol):";
+            std::cin>>n;
+            std::cout<<"Nhap khoi luong(g):";
+            std::cin>>m;
+            symbol();
+            show = m/n;
+            symbol();
+            printf("KQ:%0.2fg/mol\n",show);
+            symbol();
+            usr1();
+            break;
+
+            case 2:
+            double M;
+            std::cout<<"Nhap khoi luong(g):";
+            std::cin>>m;
+            std::cout<<"Nhap khoi luong mol(g/mol):";
+            std::cin>>M;
+            symbol();
+            show = m/M;
+            printf("KQ:%0.2fmol\n",show);
+            symbol();
+            usr1();
+            break;
+
+            case 3:
+            std::cout<<"Nhap so mol:";
+            std::cin>>n;
+            std::cout<<"Nhap khoi luong(g):";
+            std::cin>>m;
+            symbol();
+            show = n*m;
+            printf("KQ:%0.2fg\n",show);
+            symbol();
+            usr1();
+            break;
+
+            case 4:
+            std::cout<<"Nhap so mol(mol):";
+            std::cin>>n;
+            symbol();
+            show = n*24.79;
+            printf("KQ:%0.2fl\n",show);
+            symbol();
+            usr1();
+            break;
+
+            case 5:
+            std::cout<<"Nhap the tich mol cua chat khi(l):";
+            std::cin>>n;
+            symbol();
+            show=n/24.79;
+            printf("KQ:%0.2fmol\n",show);
+            symbol();
+            usr1();
+        }
     }
 };
-void choose()
+void usr()
 {
-    mol *hh = new mol();
-    std::string donvi;
-    double show;
-    int choos3;
-    std::cout<<"1.Tinh do tan cua chat trong nuoc\n"
-             <<"2.Tinh nong do phan tram\n"
-             <<"3.Tinh nong do mol\n"
-             <<"4.Tinh khoi luong dung dich\n"
-             <<"5.Tinh so mol\n"
+    mol *test = new mol();
+    int choose;
+    std::cout<<"1.Cac bai tap ve mol\n"
              <<"0.Thoat\n"
              <<"=>";
-    std::cin>>choos3;
+    std::cin>>choose;
     symbol();
-    switch (choos3)
+    switch(choose)
     {
         case 0:
         exit(0);
         break;
 
         case 1:
-        double mct,mn;
-        std::cout<<"Nhap khoi luong chat tan:";
-        std::cin>>mct;
-        std::cout<<"Nhap khoi luong nuoc:";
-        std::cin>>mn;
-        std::cout<<"Nhap don vi:";
-        std::cin>>donvi;
-        symres();
-        show = hh->tinhdotan(mn,mct);
-        std::cout<<"KQ:"<<show<<donvi<<"\n";
-        symbol();
-        choose();
-        break;
-
-        case 2:
-        double mdd;
-        std::cout<<"Nhap khoi luong chat tan:";
-        std::cin>>mct;
-        std::cout<<"Nhap khoi luong:";
-        std::cin>>mdd;
-        symres();
-        show = hh->ndpt(mct,mdd);
-        std::cout<<"KQ:"<<show<<"%"<<"\n";
-        symbol();
-        choose();
-        break;
-
-        case 3:
-        double vdd;
-        std::cout<<"Nhap so mol chat tan:";
-        std::cin>>mct;
-        std::cout<<"Nhap the tich dung dich:";
-        std::cin>>vdd;
-        std::cout<<"Nhap don vi:";
-        std::cin>>donvi;
-        symres();
-        show = hh->ndmol(mct,vdd);
-        std::cout<<"KQ:"<<show<<donvi<<"\n";
-        symbol();
-        choose();
-        break;
-
-        case 5:
-        double s1,s2,s3,sm;
-        std::cout<<"Nhap so thu 1:";
-        std::cin>>s1;
-        std::cout<<"Nhap so thu 2:";
-        std::cin>>s2;
-        std::cout<<"Nhap so mu:";
-        std::cin>>sm;
-        symres();
-        s3 = s1*std::pow(s2,sm);
-        show = hh->smol(s3);
-        std::cout<<"KQ:";
-        printf("%0.2f%s\n",show,"mol");
-        symbol();
-        choose();
+        test->usr1();
         break;
     }
 }
 int main()
 {
-    choose();
+    usr();
 }
